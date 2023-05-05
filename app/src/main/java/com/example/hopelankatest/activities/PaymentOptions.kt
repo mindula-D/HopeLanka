0e75f50605f4830b9ad64071cae74ebf84a1ce99
Mindula Deshapriya <IT21291432@my.sliit.lk>
2023-05-05 07:14:45 +0530

    updated PaymentOptions.kt

--- app/src/main/java/com/example/hopelankatest/activities/PaymentOptions.kt
+++ app/src/main/java/com/example/hopelankatest/activities/PaymentOptions.kt
@@ -13,17 +13,23 @@ class PaymentOptions : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_options)
 
+        //get the value of amount from EnterAmount
         val amount = intent.getStringExtra("amount")
+
+        // find the total textview
         val total = findViewById<TextView>(R.id.total).apply {
+            //assign the value of amount
             text = "Rs. " + amount + ".00/="
         }
 
+        // Set a click listener for the payNow button
         val payNow = findViewById<Button>(R.id.payNow)
         payNow.setOnClickListener{
             val intent = Intent(this, PaymentSuccess::class.java)
             startActivity(intent)
         }
 
+        // Set a click listener for the back button
         val imageView = findViewById<ImageView>(R.id.back)
         imageView.setOnClickListener{
             val intent = Intent(this, PreviouslyAddedCards::class.java)
