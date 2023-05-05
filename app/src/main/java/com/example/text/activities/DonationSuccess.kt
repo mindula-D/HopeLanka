28f8272dfdc0bf6a29ee5dc76f7609bf794d1b13
Dilshika20 <kumarasinghedilshika20@gmail.com>
2023-05-05 17:03:39 +0530

    updated DonationSuccess.kt

--- app/src/main/java/com/example/text/activities/DonationSuccess.kt
+++ app/src/main/java/com/example/text/activities/DonationSuccess.kt
@@ -12,14 +12,18 @@ class DonationSuccess : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donation_success)
 
+        // Find the ImageView with id `home` in the layout
         val imageView = findViewById<ImageView>(R.id.home)
 
+        // Set a click listener on the `imageView` object to navigate to `Itemm` activity
         imageView.setOnClickListener {
             val intent = Intent(this, Itemm::class.java)
             startActivity(intent)
         }
 
+        // Find the Button with id `ok` in the layout
         val doneee = findViewById<Button>(R.id.ok)
+        // Set a click listener on the `doneee` object to navigate to `Itemm` activity
         doneee.setOnClickListener {
             val intent = Intent(this, Itemm::class.java)
             startActivity(intent)
