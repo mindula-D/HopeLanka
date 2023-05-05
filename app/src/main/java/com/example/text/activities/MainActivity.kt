a8272a53ff1ace8295e95176a779d01cdcfa62e3
Dilshika20 <kumarasinghedilshika20@gmail.com>
2023-05-05 16:51:27 +0530

    Spinner updated

--- app/src/main/java/com/example/text/activities/MainActivity.kt
+++ app/src/main/java/com/example/text/activities/MainActivity.kt
@@ -15,13 +15,18 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Create an array of donation types
         val Donation =arrayOf("Clothes","Food","Education","Electronics","Medicine")
+        // Find the spinner view in the layout file and set its adapter to use the donation array
         val spinner = findViewById<Spinner>(R.id.spinner2)
         val arrayAdapter =ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,
             Donation)
         spinner.adapter=arrayAdapter
+
+        // Set an item selected listener for the spinner
         spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // When an item is selected, display a toast message showing the selected donation type
             Toast.makeText(applicationContext,"Selected donation type is = "+ Donation[position],Toast.LENGTH_SHORT ).show()
             }
 
