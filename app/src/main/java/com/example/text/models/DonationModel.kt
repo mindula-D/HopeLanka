b2510175a7d4773c1ce1cc86e09860e8d5e0b079
Dilshika20 <kumarasinghedilshika20@gmail.com>
2023-05-05 17:07:59 +0530

    Updated DonationModel.kt

--- app/src/main/java/com/example/text/models/DonationModel.kt
+++ app/src/main/java/com/example/text/models/DonationModel.kt
@@ -1,6 +1,8 @@
 package com.example.text.models
 
+// Define a data class called "DonationModel" to represent a donation
 data class DonationModel (
+    // Define properties for the donation, with default values of null
     var donationId: String? = null,
     var Name: String? = null,
     var Number: String? = null,
