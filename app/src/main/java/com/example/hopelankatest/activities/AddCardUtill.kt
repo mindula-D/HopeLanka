44495b6a02f284820cb7d088697cf6a81612ea1c
Mindula Deshapriya <IT21291432@my.sliit.lk>
2023-05-05 19:19:23 +0530

    added AddCardUtill.kt

---
+++ app/src/main/java/com/example/hopelankatest/activities/AddCardUtill.kt
@@ -0,0 +1,29 @@
+package com.example.hopelankatest.activities
+
+object AddCardUtill {
+
+    private val existingCards = listOf("1234567891234567")
+
+    fun validateAddCardInput(
+        cardNumber: String,
+        month: String,
+        year: String,
+        cvv: String,
+        cardHolderName: String,
+        bankName: String
+    ): Boolean {
+        if (cardNumber.isEmpty() || month.isEmpty() || year.isEmpty() || cvv.isEmpty() || cardHolderName.isEmpty() || bankName.isEmpty()) {
+            return false
+        }
+        if (cardNumber in existingCards){
+            return false
+        }
+        if(month > 12.toString()){
+            return false
+        }
+        if (year.count{it.isDigit()} > 2){
+            return false
+        }
+        return true
+    }
+}
