d95da99ff9270b90840abc0623f312ffd61af2a1
JayadinuDias <lakshandias12@gmail.com>
2023-05-08 00:15:14 +0530

    Testing done - Instrumental testing 2

---
+++ app/src/androidTest/java/com/example/splashscreen/activities/StringAvailabilityTest.kt
@@ -0,0 +1,34 @@
+package com.example.splashscreen.activities
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.*
+import org.junit.Before
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import com.example.splashscreen.R
+import org.junit.After
+
+class StringAvailabilityTest {
+    private lateinit var stringAvailability: StringAvailability
+
+    @Before
+    fun setup() {
+        stringAvailability = StringAvailability()
+    }
+    @Test
+    fun stringAvailabilityGivenAsSameString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = stringAvailability.isEqual(context, R.string.app_name, "SplashScreen")
+        assertThat(result).isFalse() }
+    @Test
+    fun stringAvailabilityDifferentAsGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = stringAvailability.isEqual(context, R.string.app_name, "JD")
+        assertThat(result).isFalse()
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+}
